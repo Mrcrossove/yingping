dc2a8f199f3c8c4073ddb1b0368baad42f08a267
Mrcrossove <1741606537@qq.com>
2026-06-01 13:53:22 +0800

    fix: OrderModule 导入 WebsocketModule 解决依赖注入失败

--- server/src/modules/order/order.module.ts
+++ server/src/modules/order/order.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
+import { WebsocketModule } from '../websocket/websocket.module';
 
 @Module({
+  imports: [forwardRef(() => WebsocketModule)],
   controllers: [OrderController],
   providers: [OrderService],
   exports: [OrderService],
